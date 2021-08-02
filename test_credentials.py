a831a175e99ad8912aea0d60f0c4e9648ec470ac
kodhanjo <felix.osambo@student.moringaschool.com>
2021-08-02 07:46:13 +0300

    addded docstrings to test credentials

---
+++ test_credentials.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+
+
+class Test(TestCase):
+    """
+    The test will be done in User class because it extends Credentials
+    """
+    def test_credentials(self):
+        self.fail()
