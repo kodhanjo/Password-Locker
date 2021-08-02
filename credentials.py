c77f06926f628c264179efae83ddf3d693b1bf6d
kodhanjo <felix.osambo@student.moringaschool.com>
2021-08-02 07:30:24 +0300

    added credentials.py codes

---
+++ credentials.py
@@ -0,0 +1,62 @@
+from string import ascii_letters, digits, punctuation
+from secrets import choice, token_hex
+
+
+class Credentials:
+
+    def __init__(self):
+        self.accounts = []
+
+    def generate_password(self, password_length=None):
+        """
+        Generates an alphanumeric password whose length is specified in the argument
+        :param password_length:
+        :return: string whose length is `password_len`
+        """
+        alphabets = ascii_letters + digits + punctuation
+        if password_length is None:
+            password_length = int(input('Specify the length of password you wish to generate (positive int): '))
+        else:
+            password_length = int(password_length)
+        while True:
+            password = ''.join(choice(alphabets) for i in range(password_length))
+            try:
+                accept = int(
+                    input(f"Generated password is {password}.\n Reply with 1 to accept or 2 to generate another "
+                          f"password\n"))
+                if accept == 1:
+                    return password
+                else:
+                    continue
+            except ValueError:
+                return f"You've provided an invalid input."
+
+    @property
+    def generate_token(self):
+        """
+        Create an access token for authentication of the user
+        :return: Hexadecimal srting that is url safe
+        """
+        return token_hex(16)
+
+    def add_account(self, account_name, username, password):
+        account_details = {'account_name': account_name, 'username': username, 'password': password}
+        self.accounts.append(account_details)
+        return account_details
+
+    def delete_account(self, account_name):
+        """
+        Removes account with specified account_name from accounts if the user
+        :param account_name:
+        :return:
+        """
+        if any(account['account_name'] == account_name for account in self.accounts):
+            user_input = input(f'Are you sure you want to delete your {account_name} account? (y/n): ').lower()
+            if user_input == 'y':
+                self.accounts = [ac for ac in self.accounts if not (ac.get('account_name') == account_name)]
+                print("Account was deleted successfully")
+                return self.accounts
+            else:
+                print('No account was deleted.')
+        else:
+            print("Account Not Found")
